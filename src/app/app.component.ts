bc32e35fa9f8b247541c154ffd59c47ed99081fb
vesin philippe <pvesin@sapiens.biz>
2018-01-05 13:50:25 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  styles:[]
+})
+export class AppComponent {
+  serverElements= [{type : 'server', name: 'Testserver', content: 'just a test!'}];
+}
